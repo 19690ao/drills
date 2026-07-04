d408d279e561d30504352aa17708caa96b1a3c3c
19690ao <19690ao@gmail.com>
2026-07-04 11:28:04 -0400

    Add fractions to drill registry

--- drills/index.js
+++ drills/index.js
@@ -3,9 +3,9 @@ export const DRILLS = [
   'division',
   'squares',
   `square_roots`,
+  'fractions',
   // 'addition',
   // 'percents',
-  // 'fractions',
 ];
 //   // { id: 'addition',  name: 'Addition',  icon: '+', color: '#8b5cf6', rgb: '139,92,246',  desc: 'Sums to 20',        count: '60 facts'  },
 // ];
